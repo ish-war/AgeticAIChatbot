e56666224dbb5afd539e931d37e535f8d2d13427
ishwar <ishwar.ambad@gmail.com>
2025-08-10 16:32:11 +0530

    added web search by tavily

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,6 +1,9 @@
 from langgraph.graph import StateGraph, START, END
 from src.langgraphagenticai.state.state import State
 from src.langgraphagenticai.nodes.basic_chatbot_node import BasicChatbotNode
+from src.langgraphagenticai.tools.search_tool import get_tools, create_tool_node
+from langgraph.prebuilt import tools_condition, ToolNode
+from src.langgraphagenticai.nodes.chatbot_with_tool_node import ChatbotWithToolNode
 
 
 class GraphBuilder :
@@ -20,6 +23,36 @@ class GraphBuilder :
         self.graph_builder.add_edge(START, "chatbot")
         self.graph_builder.add_edge("chatbot", END)
 
+    # function for tavily api tool 
+    def chatbot_with_tools_build_graph(self) :
+        """
+        Builds an advanced chatbot graph with tool integration. 
+        This method creates a chatbot graph that includes both a chatbot node and a tool node. It defines tools, initializes the chatot with tool capabilities, and sets up conditional and direct edges between nodes. The chatbot node is set as the entry point. 
+        """
+
+        #define tool and tool node
+        tools = get_tools()
+        tool_node = create_tool_node(tools) 
+
+        # define llm
+        llm = self.llm
+
+        # define the chatbot node
+        obj_chatbot_with_node = ChatbotWithToolNode(llm)
+        chatbot_node = obj_chatbot_with_node.create_chatbot(tools)
+
+        # add nodes 
+        self.graph_builder.add_node("chatbot", chatbot_node)
+        self.graph_builder.add_node("tools", tool_node)
+
+        # define conditional and direct edges 
+        self.graph_builder.add_edge(START, "chatbot")
+        self.graph_builder.add_conditional_edges("chatbot", tools_condition)
+        self.graph_builder.add_edge("tools", "chatbot")
+        self.graph_builder.add_edge("chatbot", END)
+
+
+
 
     def setup_graph(self, usecase: str) :
         """
@@ -28,6 +61,10 @@ class GraphBuilder :
         """
         if usecase == "Basic Chatbot" :
             self.basic_chatbot_build_graph()
+
+        if usecase == "Chatbot with Web" :
+            self.chatbot_with_tools_build_graph()
+
 
         return self.graph_builder.compile()
     
--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -35,5 +35,11 @@ class loadStreamlitUI :
             # use case selection
             self.user_controls["selected_usecase"] = st.selectbox("Select Usecases", usecase_options)
 
+            if self.user_controls["selected_usecase"] == "Chatbot with Web"  :
+                os.environ["TAVILY_API_KEY"]=self.user_controls["TAVILY_API_KEY"]=st.session_state["TAVILY_API_KEY"]= st.text_input("TAVILY_API_KEY", type= "password")
+                # Validate API key
+                if not self.user_controls["TAVILY_API_KEY"] :
+                    st.warning("Please enter your TAVILY_API_KEY to proceed. Don't have ? refer to https://www.tavily.com/" )
+
         return self.user_controls
         
